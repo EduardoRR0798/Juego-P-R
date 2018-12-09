a041fa685db9c12592d0856fbb822c07c877dc19
EduardoRR0798 <36748677+EduardoRR0798@users.noreply.github.com>
2018-12-09 13:58:14 -0600

    Pregunta, Respuesta
    Nuevos atributos y nuevos métodos.

--- JuegoPreguntantas/src/entity/Respuesta.java
+++ JuegoPreguntantas/src/entity/Respuesta.java
@@ -5,7 +5,19 @@
  */
 package entity;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,6 +29,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -49,7 +62,9 @@ public class Respuesta implements Serializable {
     @JoinColumn(name = "idpregunta", referencedColumnName = "idpregunta")
     @ManyToOne
     private Pregunta idpregunta;
-
+    @Transient
+    private byte[] imagen;
+    
     public Respuesta() {
         this.puntaje = 0;
     }
@@ -107,12 +122,14 @@ public class Respuesta implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+        
         if (!(object instanceof Respuesta)) {
             return false;
         }
         Respuesta other = (Respuesta) object;
-        if ((this.idrespuesta == null && other.idrespuesta != null) || (this.idrespuesta != null && !this.idrespuesta.equals(other.idrespuesta))) {
+        if ((this.idrespuesta == null && other.idrespuesta != null) || 
+                (this.idrespuesta != null && 
+                !this.idrespuesta.equals(other.idrespuesta))) {
             return false;
         }
         return true;
@@ -123,4 +140,66 @@ public class Respuesta implements Serializable {
         return "entity.Respuesta[ idrespuesta=" + idrespuesta + " ]";
     }
     
+    /**
+     * Este metodo crea una imagen para guardarla en un directorio.
+     * @param ruta nueva ruta de la imagen.
+     */
+    public void crearImagen(String ruta) throws IOException {
+        
+        OutputStream out = null;
+        try {
+            
+            out = new BufferedOutputStream(new FileOutputStream(ruta));
+            out.write(imagen);
+        } catch (FileNotFoundException ex) {
+            
+            Logger.getLogger(Respuesta.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            
+            Logger.getLogger(Respuesta.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        } finally {
+            if (out != null) {
+                
+                out.close();
+            }
+        }
+    }
+
+    /**
+     * Este metodo crea un arreglo de bytes para poder ser enviado por nodeJs
+     * @return imagen convertida en un arreglo de bytes.
+     */
+    public byte[] crearArregloImagen() {
+        
+        DataBufferByte data = null;
+        try {
+            
+            File imgPath = new File(respuesta);
+            BufferedImage bufferedImage = ImageIO.read(imgPath);
+
+            WritableRaster raster = bufferedImage.getRaster();
+            data = (DataBufferByte) raster.getDataBuffer();
+        } catch (IOException ex) {
+            
+            Logger.getLogger(Respuesta.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return ( data.getData() );
+    }
+    
+    /**
+     * Este metodo verifica se la respuesta es la correcta.
+     * @return true si la resuesta es correcta sino regresa false.
+     */
+    public boolean esCorrecta() {
+        
+        boolean correcta = false;
+        if(puntaje == 1) {
+            
+            correcta = true;
+        }
+        return correcta;
+    }
 }
